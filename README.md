44afd526f35938b9eac2fa1c380097bcee76e375
besser <68145204+besser435@users.noreply.github.com>
2026-02-07 02:49:27 -0700

    Add plushie on bed feature

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 
 # Player Plushies Mod
 
-
-
 ## TODO (in order):
 * Bugs (urgent)
   * Fix outer skin layer. Should it be closer to the model?
   * Support Steve model. Alex works fine, but Steve has holes. Verify this because I only tested one skin.
   * Fix different pixel size. The head which is 1:1 scale, everything else which is smaller, and then the side 
   of the torso is two pixels wide.
+  * Fix rotation states. We want 16 different rotation states like a sign or player head. To use more model states other than 
+  90 degree increments, we need either a block entity renderer, or use different models that have baked in rotation. Since we
+  want to avoid being a block entity, we will probably need to bake different models. <br>Revisit this later. If we 
+  decide to use a BE after all, use a BER. Otherwise, bake different models. <br>BER would also allow for lowering the
+  model into a bed without a duplicate model, using just code.
+
 * Features
   * Water loggable 
   * Default skin/texture
   * 🏳️‍⚧️ fluffy shark plushie (blahaj)
-  * If placed on bed, use a model that is lower on the Y coordinate so it looks like the plushie is on the bed nicely
+
 * Stretch features
   * Custom holding animation like how some blahaj mods use the crossbow position
   * Support older versions of NeoForge
--- src/main/java/me/besser/playerplushies/blocks/playerPlushie/PlayerPlushieBlock.java
+++ src/main/java/me/besser/playerplushies/blocks/playerPlushie/PlayerPlushieBlock.java
@@ -2,6 +2,7 @@ package me.besser.playerplushies.blocks.playerPlushie;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
@@ -25,50 +26,67 @@ import org.jetbrains.annotations.NotNull;
 public class PlayerPlushieBlock extends Block implements SimpleWaterloggedBlock {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-
+    public static final BooleanProperty ON_BED = BooleanProperty.create("on_bed");
 
     // FIXME: Hitbox does not change according to model rotation.
-    protected static final VoxelShape SHAPE = Block.box(
-            4.0D, 0.0D, 6.0D,
-            12.0D, 16.0D, 14.0D);
+    protected static final VoxelShape SHAPE_NORMAL = Block.box(
+            4.0D, 0.0D, 4.0D,
+            12.0D, 16.0D, 12.0D
+    );
+
+    protected static final VoxelShape SHAPE_ON_BED = Block.box(
+            4.0D, 0.0D, 4.0D,
+            12.0D, 9.0D, 12.0D
+    );
 
     public PlayerPlushieBlock(Properties properties) {
         super(properties.noOcclusion());
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(FACING, Direction.NORTH)
-                .setValue(WATERLOGGED, false));
+                .setValue(WATERLOGGED, false)
+                .setValue(ON_BED, false));
     }
 
     @Override
-    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+    public @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return state.getValue(ON_BED) ? SHAPE_ON_BED : SHAPE_NORMAL;
     }
 
-    // This logic tells Minecraft to face the block TOWARDS the player when placed
+
+    // Block states
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockPos pos = context.getClickedPos();
         FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
+        boolean isBed = context.getLevel().getBlockState(pos.below()).is(BlockTags.BEDS);
 
         return this.defaultBlockState()
                 .setValue(FACING, context.getHorizontalDirection().getOpposite())
-                .setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+                .setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER)
+                .setValue(ON_BED, isBed);
     }
 
-    @Override   // Render water correctly
-    public @NotNull FluidState getFluidState(BlockState state) {
-        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-    }
-
-    @Override   // Change waterlogged state on block update
+    @Override
     public @NotNull BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
         if (state.getValue(WATERLOGGED)) {
             level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
         }
+
+        if (facing == Direction.DOWN) {
+            boolean isBed = level.getBlockState(facingPos).is(BlockTags.BEDS);
+            return state.setValue(ON_BED, isBed);
+        }
+
         return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
     }
 
     @Override
+    public @NotNull FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING, WATERLOGGED);
+        builder.add(FACING, WATERLOGGED, ON_BED);
     }
 }
